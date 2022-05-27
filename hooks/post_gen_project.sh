d4ea58f300a205c012699129d079b89ae77dc4ae
Javed, Danish <Danish.Javed@withsecure.com>
2022-05-27 17:17:24 +0100

    fix: add validation for project name

--- hooks/post_gen_project.sh
+++
@@ -1,4 +0,0 @@
-cd {{cookiecutter.project_slug}}
-git init
-git add .
-git commit -am "feat: first commit"
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -10,3 +10,5 @@ if not re.match(MODULE_REGEX, module_name):
     print('ERROR: %s is not a valid Python module name!' % module_name)
     # exits with status 1 to indicate failure
     sys.exit(1)
+else:
+    print('Post Creation Validation: Everything looks good')

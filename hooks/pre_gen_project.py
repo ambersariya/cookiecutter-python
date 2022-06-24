76b87ea924ab58161533156f50cff5ebb119c1f1
Danish Javed <1080238+ambersariya@users.noreply.github.com>
2022-06-24 11:24:09 +0100

    Update pre_gen_project.py

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,10 +4,10 @@ import sys
 
 MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
 
-module_name = '{{ cookiecutter.module_name }}'
+package_slug = '{{ cookiecutter.package_slug }}'
 
-if not re.match(MODULE_REGEX, module_name):
-    print('ERROR: %s is not a valid Python module name!' % module_name)
+if not re.match(MODULE_REGEX, package_slug):
+    print('ERROR: %s is not a valid Python module name!' % package_slug)
     # exits with status 1 to indicate failure
     sys.exit(1)
 else:
